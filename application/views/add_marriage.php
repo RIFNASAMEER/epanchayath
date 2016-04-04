e8baf7f417324a4c82de0c0f2e95e28c57bc73fd
rifnasameer <fifnarintoos@gmail.com>
2016-04-04 14:41:17 +0530

    mqrriege

--- application/views/add_marriage.php
+++ application/views/add_marriage.php
@@ -3,59 +3,57 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>application for marriage certificate</title>
+	<title>application for marriage registration</title>
 	<link rel="stylesheet" href="">
 </head>
 <body>
 	<form action="" method="post" accept-charset="utf-8">
 
-		<label for="date">Date of Marriage<input type="text" name="date" value="date_of_mariage"></label>
-		<label for="place">Place of Marriage<input type="text" name="" value="place"></label>
+		<label for="date">Date of Marriage<div><input type="text" name="date" value="date"></div></label>
+		<label for="place">Place of Marriage<div><input type="text" name="" value="place"></div></label>
 
 		<h6>husband details</h6>
 
-		<label for="name">Name<input type="text" name="name" id="name"></label>
-		<label for="address">Address<input type="text" name="address" id="address"></label>
-		<label for="dob">DOB<select name="dob">
+		<label for="name">Name<div><input type="text" name="name" id="name"></div></label>
+		<label for="address">Address<div><input type="text" name="address" id="address"></div></label>
+		<label for="dob">DOB<div><select name="dob">
 			<option value="1">1</option>
-			</select></label><br>
+			</select></div></label><br>
 			
-		<label for="citizen">citizen<input type="text" name="citizen" id="citizen"></label>
+		<label for="citizen">citizen<div><input type="text" name="citizen" id="citizen"></div></label>
 		<label for="job">Job<input type="text" name="job" id="job"></label>
 		<label for="martial_status">First Married or not<input type="radio" name="status" id="first_married_or_not" value="yes">yes<input type="radio" name="status" id="first_married_or_not" value="no">no</label>
-		<label for="no of spouse">Number of Spouse<input type="text" name="no_spouse" id="any_spouse_are_living"></label>
-		<label for="father_name">Father Name<input type="text" name="f_name" id="f_name"></label>
-		<label for="age">Age<input type="text" name="f_age" id="f_age"></label>
-		<label for="address">Address<input type="text" name="f_address" id="f_address"></label>
-		<label for="mother">mother Name<input type="text" name="m_name" id="mother_name"></label>
-		<label for="m_age">Age<input type="text" name="m_age" id="mother_age"></label>
-		<label for="m_adres">Age<input type="text" name="m_addres" id="mother_adres"></label>
-
+		<label for="no of spouse">Number of Spouse<div><input type="text" name="no_spouse" id="any_spouse_are_living"></div></label>
+		<label for="father_name">Father Name/Name of Guardian<div><input type="text" name="f_name" id="f_name"></div></label>
+		<label for="age">Age<div><input type="text" name="f_age" id="f_age"></div></label>
+		<label for="address">Address<div><input type="text" name="f_address" id="f_address"></div></label>
+		<label for="mother">Mother Name<div><input type="text" name="m_name" id="mother_name"></div></label>
+		<label for="m_age">Age<div><input type="text" name="m_age" id="m_age"></div></label>
+		<label for="m_adres">Address<div><input type="text" name="m_addres" id="m_adres"></div></label>
+		<label for="victim_name">Victim Name<div><input type="text" name="name" id="name"></div></label>
+		<label for="v_adres">Address<div><input type="text" name="v_addres" id="v_adres"></div></label>
+		<label for="victim_name">Victim Name<div><input type="text" name="name" id="name"></div></label>
+		<label for="v_adres">Address<div><input type="text" name="v_addres" id="v_adres"></div></label>
         <h6>wife details<h6>
 
-		<label for="name">Name<input type="text" name="name" id="name"></label>
-		<label for="address">Address<input type="text" name="address" id="address"></label>
-		<label for="dob">DOB<select name="dob">
+		<<label for="name">Name<div><input type="text" name="name" id="name"></div></label>
+		<label for="address">Address<div><input type="text" name="address" id="address"></div></label>
+		<label for="dob">DOB<div><select name="dob">
 			<option value="1">1</option>
-			</select></label><br>
+			</select></div></label><br>
 			
-		<label for="citizen">citizen<input type="text" name="citizen" id="citizen"></label>
+		<label for="citizen">citizen<div><input type="text" name="citizen" id="citizen"></div></label>
 		<label for="job">Job<input type="text" name="job" id="job"></label>
 		<label for="martial_status">First Married or not<input type="radio" name="status" id="first_married_or_not" value="yes">yes<input type="radio" name="status" id="first_married_or_not" value="no">no</label>
-		<label for="no_of_spouse">Number of Spouse<input type="text" name="no_spouse" id="no_of_spouse"></label>
-		<label for="father_name">Father Name<input type="text" name="f_name" id="f_name"></label>
-		<label for="age">Age<input type="text" name="f_age" id="f_age"></label>
-		<label for="address">Address<input type="text" name="f_address" id="f_address"></label>
-		<label for="mother">mother Name<input type="text" name="m_name" id="mother_name"></label>
-		<label for="m_age">Age<input type="text" name="m_age" id="mother_age"></label>
-		<label for="m_adres">Age<input type="text" name="m_addres" id="mother_adres"></label>
-
-        <label for="victim1_name">First victim Name<input type="text" name="vname" id="name"></label>
-		<label for="victim1_address">Victim Address<input type="text" name="vaddress" id="address"></label>
-		<label for="victim2_name">Name<input type="text" name="name" id="2name"></label>
-		<label for="victim2_address">Address<input type="text" name="2address" id="address"></label>
-
-
+		<label for="no of spouse">Number of Spouse<div><input type="text" name="no_spouse" id="any_spouse_are_living"></div></label>
+		<label for="father_name">Father Name/Name of Guardian<div><input type="text" name="f_name" id="f_name"></div></label>
+		<label for="age">Age<div><input type="text" name="f_age" id="f_age"></div></label>
+		<label for="address">Address<div><input type="text" name="f_address" id="f_address"></div></label>
+		<label for="mother">Mother Name<div><input type="text" name="m_name" id="mother_name"></div></label>
+		<label for="m_age">Age<div><input type="text" name="m_age" id="m_age"></div></label>
+		<label for="m_adres">Address<div><input type="text" name="m_addres" id="m_adres"></div></label>
+		<label for="victim_name">Name<div><input type="text" name="name" id="name"></div></label>
+		<label for="v_adres">Address<div><input type="text" name="v_addres" id="v_adres"></div></label>
 	</form>
 </body>
 </html>
