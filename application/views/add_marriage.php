79b4b5c5653c07ef2d034c0c23bb1d1aaca252ca
rifnasameer <fifnarintoos@gmail.com>
2016-04-04 13:10:10 +0530

    nothing

---
+++ application/views/add_marriage.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>application for marriage certificate</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<form action="" method="post" accept-charset="utf-8">
+
+		<label for="date">Date of Marriage<input type="text" name="date" value="date_of_mariage"></label>
+		<label for="place">Place of Marriage<input type="text" name="" value="place"></label>
+
+		<h6>husband details</h6>
+
+		<label for="name">Name<input type="text" name="name" id="name"></label>
+		<label for="address">Address<input type="text" name="address" id="address"></label>
+		<label for="dob">DOB<select name="dob">
+			<option value="1">1</option>
+			</select></label><br>
+			
+		<label for="citizen">citizen<input type="text" name="citizen" id="citizen"></label>
+		<label for="job">Job<input type="text" name="job" id="job"></label>
+		<label for="martial_status">First Married or not<input type="radio" name="status" id="first_married_or_not" value="yes">yes<input type="radio" name="status" id="first_married_or_not" value="no">no</label>
+		<label for="no of spouse">Number of Spouse<input type="text" name="no_spouse" id="any_spouse_are_living"></label>
+		<label for="father_name">Father Name<input type="text" name="f_name" id="f_name"></label>
+		<label for="age">Age<input type="text" name="f_age" id="f_age"></label>
+		<label for="address">Address<input type="text" name="f_address" id="f_address"></label>
+		<label for="mother">mother Name<input type="text" name="m_name" id="mother_name"></label>
+		<label for="m_age">Age<input type="text" name="m_age" id="mother_age"></label>
+		<label for="m_adres">Age<input type="text" name="m_addres" id="mother_adres"></label>
+
+        <h6>wife details<h6>
+
+		<label for="name">Name<input type="text" name="name" id="name"></label>
+		<label for="address">Address<input type="text" name="address" id="address"></label>
+		<label for="dob">DOB<select name="dob">
+			<option value="1">1</option>
+			</select></label><br>
+			
+		<label for="citizen">citizen<input type="text" name="citizen" id="citizen"></label>
+		<label for="job">Job<input type="text" name="job" id="job"></label>
+		<label for="martial_status">First Married or not<input type="radio" name="status" id="first_married_or_not" value="yes">yes<input type="radio" name="status" id="first_married_or_not" value="no">no</label>
+		<label for="no_of_spouse">Number of Spouse<input type="text" name="no_spouse" id="no_of_spouse"></label>
+		<label for="father_name">Father Name<input type="text" name="f_name" id="f_name"></label>
+		<label for="age">Age<input type="text" name="f_age" id="f_age"></label>
+		<label for="address">Address<input type="text" name="f_address" id="f_address"></label>
+		<label for="mother">mother Name<input type="text" name="m_name" id="mother_name"></label>
+		<label for="m_age">Age<input type="text" name="m_age" id="mother_age"></label>
+		<label for="m_adres">Age<input type="text" name="m_addres" id="mother_adres"></label>
+
+        <label for="victim1_name">First victim Name<input type="text" name="vname" id="name"></label>
+		<label for="victim1_address">Victim Address<input type="text" name="vaddress" id="address"></label>
+		<label for="victim2_name">Name<input type="text" name="name" id="2name"></label>
+		<label for="victim2_address">Address<input type="text" name="2address" id="address"></label>
+
+
+	</form>
+</body>
+</html>
